3722be723d70d7b6bad8b561cc6c8874e5e7b683
mkayf <muhammadkaif150@gmail.com>
2025-05-16 00:45:45 +0500

    browser recipes button fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Button} from './index'
 import HeroImg1 from '../assets/hero-img.jpg'
 import HeroImg2 from '../assets/hero-img-2.jpg'
+import { Link } from 'react-router-dom'
 
 
 function Hero() {
@@ -10,7 +11,9 @@ function Hero() {
       <div className='flex-1/12 flex items-start justify-center flex-col'>
         <h1 className='text-white poppins-semibold text-3xl md:leading-12 lg:text-5xl lg:leading-16'>Fuel Your Body with Easy & Healthy Recipes!</h1>
         <p className='text-white poppins-regular leading-7 mt-4'>Explore a world of easy-to-make, nutritious recipes that bring health and happiness to your plate.</p>
-        <Button text='Browse Recipes' className='mt-4' />
+        <Link to="posts">
+          <Button text='Browse Recipes' className='mt-4'  />
+        </Link>
       </div>
       <div className='flex-1 relative hidden md:flex h-70 lg:h-100'>
         <img src={HeroImg2} className='h-full rounded-xl shadow-xl object-cover relative z-10' alt='A woman cooking healthy food'/>
